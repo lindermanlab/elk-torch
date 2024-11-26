6813cd12b66e046b42a36b809b291f1079e3219f
Xavier Gonzalez <xavier18@stanford.edu>
2024-11-25 17:02:43 -0800

    minor (quick-push)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="elk-torch",
+    name="elk_torch",
     version="0.1",
     packages=find_packages(),
     install_requires=[],  # Add core dependencies here if needed
