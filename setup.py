7595d74ab35fc2ab97f3e3f80f6c0f4fc27baac4
Xavier Gonzalez <xavier18@stanford.edu>
2024-11-25 16:52:42 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="elk-torch",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add core dependencies here if needed
+)
